33f5db2a5ada85ae36c154c0cacd2343037ea2c7
lindam <linda.minotti2@gmail.com>
2020-07-10 12:35:42 +0200

    Jumbotron responsive.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/js/app.js', 'dist/').sass('src/scss/app.scss', 'dist/');
+mix.js('src/js/app.js', 'dist/').sass('src/scss/app.scss', 'dist/').options({processCssUrls: false});
 
 // Full API
 // mix.js(src, output);
